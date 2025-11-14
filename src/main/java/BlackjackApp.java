26a6910081e10e3639b165211082fa0910c39613
Chris Coulbeck <ccoulbeck3@gmail.com>
2025-11-14 22:03:35 +1300

    Implement initial card dealing to player and dealer

--- src/main/java/BlackjackApp.java
+++ src/main/java/BlackjackApp.java
@@ -1,7 +1,31 @@
+import model.Card;
+import model.Deck;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlackjackApp {
     static void main() {
         BlackjackApp.play();
     }
 
-    static void play() {}
+    static void play() {
+        Deck deck = new Deck();
+
+        List<Card> playerHand = new ArrayList<>();
+        List<Card> dealerHand = new ArrayList<>();
+
+        playerHand.add(deck.drawCard());
+        playerHand.add(deck.drawCard());
+
+        dealerHand.add(deck.drawCard());
+        dealerHand.add(deck.drawCard());
+
+        System.out.printf("Your hand: %s\n", handToString(playerHand));
+        System.out.printf("Dealer's hand: %s\n", handToString(dealerHand));
+    }
+
+    private static String handToString(List<Card> hand) {
+        return hand.toString().replace("[", "").replace("]", "");
+    }
 }
